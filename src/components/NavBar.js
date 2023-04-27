61ad4469ac2131d31b6d710b70ae31414e2fc142
Yegon Japheth Kiptoo <jyegon504@gmail.com>
2023-04-27 14:06:39 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,30 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (<div className="navbar">
+            {/*{code here}*/}
+            <NavLink
+            exact
+             to='/'
+            >
+              Home
+            </NavLink><br></br>
+            <NavLink
+             to='/movies'
+            >
+              Movies
+            </NavLink><br></br>
+            <NavLink
+             to='/directors'
+            >
+              Directors
+            </NavLink><br></br>
+            <NavLink
+             to='/actors'
+            >
+              Actors
+            </NavLink><br></br>
+          </div>);
 }
 
 export default NavBar;
